87dcd5f850659dda6fef3289d3cba4fbe15ffb75
ghofrandayyat <ghofrandayyat@gmail.com>
2021-05-12 19:14:50 +0300

    add challeng 4

--- javascript/matrix/__tests__/matrix-ll.test.js
+++ javascript/matrix/__tests__/matrix-ll.test.js
@@ -6,7 +6,7 @@ const inpuArray=[ [1, 2, 3], [3, 5, 7], [1, 7, 10] ];
 const inpuArray2=[ [0, 1, 5], [-4, 7, 2], [-3, 12, 11] ];
 
 describe('Testing code challenge 4 ', () => {
-  it('It should  sum of each row.', () => {
+  it('It should return array of the sum of each row.', () => {
     expect(shift(inpuArray)).toStrictEqual([6, 15, 18]);
     expect(shift(inpuArray2)).toStrictEqual([6, 5, 20]);
 
