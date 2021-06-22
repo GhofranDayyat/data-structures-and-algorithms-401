e590d52a00c530d05389d9549e7fa7d51865d565
ghofrandayyat <ghofrandayyat@gmail.com>
2021-06-23 02:12:30 +0300

    done

---
+++ javascript/challenges/hashtable/leftJoin/leftJoin.js
@@ -0,0 +1,24 @@
+const hashTable = require('../hashtable/hashtable');
+
+
+
+module.exports= function leftJion(table1 , table2){
+  let resultArr=[];
+  for (let i = 0; i <= table1.table.length - 1; i++) {
+    if (table1.table[i]) {
+    console.log(table1.table[i]);
+      let key1 = [Object.keys(table1.table[i].head.value)[0] ] [0];
+      let key2 = [Object.keys(table2.table[i].head.value)[0] ] [0];
+      let v1 = [Object.values(table1.table[i].head.value)[0] ] [0];
+      let v2 = [Object.values(table2.table[i].head.value)[0] ] [0];
+      if(key1===key2){
+        let packet =[key1,v1,v2];
+        resultArr.push(packet);
+      }
+    }
+  }
+
+  return resultArr;
+
+};
+
